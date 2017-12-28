b0a20cef39e6297a5c957040942805224f4282fd
Dmitry Myadzelets <dmitrymyadzelets@gmail.comgit config --global user.email dmitrymyadzelets@gmail.com>
2017-12-28 15:52:33 +0100

    Simplified

--- index.js
+++ index.js
@@ -1,44 +1,41 @@
-/*jslint browser: false*/
-'use strict';
+/* jslint browser: false */
+'use strict'
 
-function on(event, fun) {
-    if (!this.hasOwnProperty('events')) {
-        throw new Error('Can\'t set event. Call emitter(object) first.');
-    }
-    if ('function' !== typeof fun) {
-        throw new Error('Event handler should be a function');
-    }
-    this.events = this.events || {};
-    this.events[event] = this.events[event] || [];
-    this.events[event].push(fun);
-    return this;
+function on (event, fun) {
+  if (!this.hasOwnProperty('events')) {
+    throw new Error('Can\'t set event. Call emitter(object) first.')
+  }
+  if (typeof fun !== 'function') {
+    throw new Error('Event handler should be a function')
+  }
+  if (this.events[event]) {
+    this.events[event].push(fun)
+  } else {
+    this.events[event] = [fun]
+  }
+  return this
 }
 
-function emit(event, a, b) {
-    if (this.events && this.events[event]) {
-        var i, m = this.events[event].length | 0;
-        for (i = 0; i < m; i += 1) {
-            switch (arguments.length) {
-            case 1:
-                this.events[event][i].call(this);
-                break;
-            case 2:
-                this.events[event][i].call(this, a);
-                break;
-            case 3:
-                this.events[event][i].call(this, a, b);
-                break;
-            default:
-                this.events[event][i].apply(this, Array.prototype.slice.call(arguments, 1));
-            }
-        }
+function emit (event, o) {
+  if (this.events[event]) {
+    var i
+    var m = this.events[event].length | 0
+    for (i = 0; i < m; i += 1) {
+      if (arguments.length === 2) {
+        this.events[event][i].call(this, o)
+      } else {
+        this.events[event][i].apply(this, Array.prototype.slice.call(arguments, 1))
+      }
     }
+  }
 }
 
-exports.emitter = function (o) {
-    o = o || {};
-    o.on = on;
-    o.emit = emit;
-    o.events = {};
-    return o;
-};
+module.exports = function (o) {
+  o = o || {}
+  o.on = on
+  o.emit = emit
+  o.events = {}
+  return o
+}
+
+module.exports.emitter = module.exports
